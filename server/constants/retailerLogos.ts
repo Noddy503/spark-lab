3e8b5b4d99bb4165d16ed6104aece08003b9efeb
Noddy503 <itsnoddy503@gmail.com>
2025-08-01 01:28:32 +0530

    Update retailerLogos.ts

--- server/constants/retailerLogos.ts
+++ server/constants/retailerLogos.ts
@@ -1,15 +1,34 @@
 // Retailer logo URLs - using reliable CDN sources
 export const RETAILER_LOGOS = {
-  apple: "https://cdn.builder.io/api/v1/image/assets%2F277789437b114a6294d98f62bce9b60e%2F35da3c4f15b240fda7e67578963c095d",
-  amazon: "https://cdn.builder.io/api/v1/image/assets%2F277789437b114a6294d98f62bce9b60e%2Fff0137610d3b48cf899ee369f5bccfe1",
-  flipkart: "https://cdn.builder.io/api/v1/image/assets%2F277789437b114a6294d98f62bce9b60e%2F24430bdf4be4433ba90377160fb8fbcf",
-  samsung: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Samsung_Logo.svg/300px-Samsung_Logo.svg.png",
-  xiaomi: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Xiaomi_logo_%282021-%29.svg/300px-Xiaomi_logo_%282021-%29.svg.png",
-  oneplus: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OnePlus_logo.svg/300px-OnePlus_logo.svg.png",
-  oppo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Oppo_logo.svg/300px-Oppo_logo.svg.png",
-  vivo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Vivo_logo_2019.svg/300px-Vivo_logo_2019.svg.png",
-  realme: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Realme_logo.svg/300px-Realme_logo.svg.png",
-  motorola: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Motorola_logo.svg/300px-Motorola_logo.svg.png",
-  google: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/120px-Google_%22G%22_logo.svg.png",
-  sony: "https://cdn.builder.io/api/v1/image/assets%2F277789437b114a6294d98f62bce9b60e%2F2cd12cb15903444a9ebd5c962b9fd1aa"
+  // Tech / Electronics Retailers
+  apple: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+  amazon: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg",
+  flipkart: "https://upload.wikimedia.org/wikipedia/commons/d/d0/Flipkart_logo.svg",
+  samsung: "https://upload.wikimedia.org/wikipedia/commons/2/24/Samsung_Logo.svg",
+  xiaomi: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Xiaomi_logo_%282021-%29.svg",
+  oneplus: "https://upload.wikimedia.org/wikipedia/commons/0/02/OnePlus_logo.svg",
+  oppo: "https://upload.wikimedia.org/wikipedia/commons/9/9d/Oppo_logo.svg",
+  vivo: "https://upload.wikimedia.org/wikipedia/commons/7/7f/Vivo_logo_2019.svg",
+  realme: "https://upload.wikimedia.org/wikipedia/commons/7/73/Realme_logo.svg",
+  motorola: "https://upload.wikimedia.org/wikipedia/commons/1/16/Motorola_logo.svg",
+  google: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg",
+  sony: "https://upload.wikimedia.org/wikipedia/commons/6/6c/Sony_Logo.svg",
+
+  // Grocery / Delivery Retailers
+  bigbasket: "https://upload.wikimedia.org/wikipedia/commons/7/7b/BigBasket_Logo.svg",
+  blinkit: "https://upload.wikimedia.org/wikipedia/commons/f/f5/Blinkit_Logo.svg",
+  zepto: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Zepto_logo.svg",
+  jiomart: "https://upload.wikimedia.org/wikipedia/commons/c/c1/JioMart_logo.svg",
+  amazonFresh: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Amazon_Fresh_logo.svg",
+  amazonPantry: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg", // fallback
+  tesco: "https://upload.wikimedia.org/wikipedia/commons/0/00/Tesco_Logo.svg",
+  sainsburys: "https://upload.wikimedia.org/wikipedia/commons/b/bc/Sainsbury%27s_logo.svg",
+  asda: "https://upload.wikimedia.org/wikipedia/commons/e/e6/ASDA_logo.svg",
+  morrisons: "https://upload.wikimedia.org/wikipedia/en/e/e9/Morrisons_2021_logo.svg",
+  kroger: "https://upload.wikimedia.org/wikipedia/commons/e/e4/Kroger_logo_2020.svg",
+  target: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Target_logo.svg",
+  swiggy: "https://upload.wikimedia.org/wikipedia/en/1/12/Swiggy_logo.svg",
+  zomato: "https://upload.wikimedia.org/wikipedia/commons/7/75/Zomato_logo_2022.svg",
+  dunzo: "https://upload.wikimedia.org/wikipedia/en/b/bd/Dunzo_logo.svg",
+  grofers: "https://upload.wikimedia.org/wikipedia/commons/f/f5/Blinkit_Logo.svg", // rebranded
 };
